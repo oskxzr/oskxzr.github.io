a95ab285121dddd7fcca6e5c107a18611df75382
oskar <oskargreig@gmail.com>
2022-10-31 17:48:28 +1300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -8,10 +8,13 @@ if (code !== null) {
     fetch('https://Chanel-Backend.oskxzr.repl.co/login?code=' + code)
         .then(response => response.json())
         .then(data => {
-            localStorage.setItem("identity", data)
-            window.location.replace("https://kats.nz");
+            console.log(data)
+            sessionStorage.setItem("identity", data)
+            sessionStorage.getItem("identity")
+            // window.location.replace("https://kats.nz")
         })
         .catch(err => {
+            console.log(response)
             console.error(err)
             body.children[0].innerHTML = "uh oh!"
             body.children[1].innerHTML = "something went wrong... please try again"
